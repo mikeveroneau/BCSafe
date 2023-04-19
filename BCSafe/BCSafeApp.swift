aa4a724f2b70db3bc10e6c4b5436aaba18e4b15d
Mike Veroneau <veronemi@bc.edu>
2023-04-19 12:30:49 -0400

    Initial Commit

---
+++ BCSafe/BCSafeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BCSafeApp.swift
+//  BCSafe
+//
+//  Created by mike on 4/19/23.
+//
+
+import SwiftUI
+
+@main
+struct BCSafeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
