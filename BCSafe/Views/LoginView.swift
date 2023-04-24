4ff8c565d6c282f8f748fa10dc13fc44c3a69a25
Mike Veroneau <veronemi@bc.edu>
2023-04-23 22:39:34 -0400

    4/23 - updated login screen to reflect Google sign-in

---
+++ BCSafe/Views/LoginView.swift
@@ -0,0 +1,216 @@
+//
+//  LoginView.swift
+//  BCSafe
+//
+//  Created by mike on 4/19/23.
+//
+
+import SwiftUI
+import Firebase
+
+struct LoginView: View {
+    @EnvironmentObject var authenticationVM: AuthenticationViewModel
+
+    @State private var presentSheet = false
+    
+    var body: some View {
+        VStack {
+            Image("logoNoBG")
+                .resizable()
+                .scaledToFit()
+                .padding()
+            
+            Text("Keeping Our Community Safer")
+                .multilineTextAlignment(.center)
+                .font(.custom("Avenir", size: 40))
+                .foregroundColor(Color("BCGold"))
+                .bold()
+                .italic()
+                .padding(.top, 50)
+            
+            Spacer()
+            
+            Button {
+                Task {
+                    presentSheet = await authenticationVM.signInWithGoogle()
+                }
+            } label: {
+                HStack {
+                    Image("google")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 30, height: 30)
+                    
+                    Text("Login With School Email")
+                        .foregroundColor(Color("BCMaroon"))
+                        .font(.system(size: 20))
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .tint(Color("BCGold"))
+            
+            Spacer()
+        }
+        .onAppear {
+            if Auth.auth().currentUser != nil {
+                //if user is already logged in before
+                print("🪵 Login Successful!")
+                presentSheet = true
+            }
+        }
+        .fullScreenCover(isPresented: $presentSheet) {
+            HomeBarView()
+        }
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+            .environmentObject(AuthenticationViewModel())
+    }
+}
+
+
+
+//import SwiftUI
+//import Firebase
+//
+//struct LoginView: View {
+//    @EnvironmentObject var authenticationVM: AuthenticationViewModel
+//
+//    enum Field {
+//        case email, password
+//    }
+//
+//    @State private var email = ""
+//    @State private var password = ""
+//    @State private var showingAlert = false
+//    @State private var alertMessage = ""
+//    @State private var butttonsDisabled = true
+//    @State private var presentSheet = false
+//    @FocusState private var focusField: Field?
+//
+//    var body: some View {
+//        VStack {
+//            Image(systemName: "photo")
+//                .resizable()
+//                .scaledToFit()
+//                .padding()
+//
+//            Group {
+//                TextField("E-mail", text: $email)
+//                    .keyboardType(.emailAddress)
+//                    .autocorrectionDisabled()
+//                    .textInputAutocapitalization(.never)
+//                    .submitLabel(.next)
+//                    .focused($focusField, equals: .email)
+//                    .onSubmit {
+//                        focusField = .password
+//                    }
+//                    .onChange(of: email) { _ in
+//                        enableButtons()
+//                    }
+//
+//                SecureField("Password", text: $password)
+//                    .textInputAutocapitalization(.never)
+//                    .submitLabel(.done)
+//                    .focused($focusField, equals: .password)
+//                    .onSubmit {
+//                        focusField = nil
+//                    }
+//                    .onChange(of: password) { _ in
+//                        enableButtons()
+//                    }
+//            }
+//            .textFieldStyle(.roundedBorder)
+//            .overlay {
+//                RoundedRectangle(cornerRadius: 5)
+//                    .stroke(.gray.opacity(0.5), lineWidth: 2)
+//            }
+//            .padding(.horizontal)
+//
+//            HStack {
+//                Button {
+//                    register()
+//                } label: {
+//                    Text("Sign Up")
+//                }
+//                .padding(.trailing)
+//
+//                Button {
+//                    login()
+//                } label: {
+//                    Text("Log In")
+//                }
+//                .padding(.leading)
+//            }
+//            .disabled(butttonsDisabled)
+//            .buttonStyle(.borderedProminent)
+//            .tint(Color("BCMaroon"))
+//            .font(.title2)
+//            .padding(.top)
+//
+//            Button {
+//                Task {
+//                    presentSheet = await authenticationVM.signInWithGoogle()
+//                }
+//            } label: {
+//                Text("Google")
+//            }
+//
+//        }
+//        .alert(alertMessage, isPresented: $showingAlert) {
+//            Button("OK", role: .cancel) {}
+//        }
+//        .onAppear {
+//            if Auth.auth().currentUser != nil {
+//                //if user is already logged in before
+//                print("🪵 Login Successful!")
+//                presentSheet = true
+//            }
+//        }
+//        .fullScreenCover(isPresented: $presentSheet) {
+//            HomeBarView()
+//        }
+//    }
+//
+//    func enableButtons() {
+//        let emailIsGood = email.count > 6 && email.contains("@bc.edu")
+//        let passwordIsGood = password.count > 6
+//        butttonsDisabled = !(emailIsGood && passwordIsGood)
+//    }
+//
+//    func register() {
+//        Auth.auth().createUser(withEmail: email, password: password) { result, error in
+//            if let error = error {
+//                print("😡 SIGN-UP ERROR: \(error.localizedDescription)")
+//                alertMessage = "SIGN-UP ERROR: \(error.localizedDescription)"
+//                showingAlert = true
+//            } else {
+//                print("😎 Registration Success!")
+//                presentSheet = true
+//            }
+//        }
+//    }
+//
+//    func login() {
+//        Auth.auth().signIn(withEmail: email, password: password) { result, error in
+//            if let error = error {
+//                print("😡 LOGIN ERROR: \(error.localizedDescription)")
+//                alertMessage = "LOGIN ERROR: \(error.localizedDescription)"
+//                showingAlert = true
+//            } else {
+//                print("🪵 Login Successful!")
+//                presentSheet = true
+//            }
+//        }
+//    }
+//}
+//
+//struct LoginView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        LoginView()
+//            .environmentObject(AuthenticationViewModel())
+//    }
+//}
